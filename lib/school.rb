18ab9e9ef12c2f90d9e70c830d3a1803af328a97
dinnosaur <dinnoroni@gmail.com>
2020-02-19 16:54:33 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,50 @@
-# code here!
+# coclasde here!
+require 'pry'
+class School 
+   
+   attr_accessor :roster
+    
+    def initialize(name)
+    @roster = {}
+    @school_name = name 
+    end 
+
+    def add_student(name, grade)
+        #need to add multiple arrays with each grade that it is assoiciated with 
+    #    switch = false  
+        
+    #     # array = []
+    #     # array << name
+    #   @roster.each do|key,value| 
+    #     if key == grade
+    #         switch = true 
+    #     end
+    #     end 
+
+
+    
+        if !@roster[grade]
+            @roster[grade] = [name]
+        else 
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+      found_grade = @roster.select{|key, value|
+            key == grade
+          }
+
+        found_grade[grade]
+    end
+
+    def sort
+        sort_students = {}
+        @roster.each do |key, value| 
+                        sort_students[key] = value.sort
+                     end
+      sort_students.sort.to_h
+    end
+    
+
+end 
